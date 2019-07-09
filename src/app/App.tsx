fb2e0970c0cece2588b8fa8d6c983435fd5cd128
SKCrawford <s.kyle.crawford@gmail.com>
2019-07-09 11:06:40 -0700

    Remove GraphQL

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,10 @@
-import { ApolloProvider } from 'react-apollo-hooks';
 import * as React from 'react';
 import { Router } from 'react-router-dom';
 
-import { client } from '../graphql';
 import { history } from '../router';
 
 export const App: React.SFC = () => (
-  <ApolloProvider client={client}>
-    <Router history={history}>
-      <div>hello, world!</div>
-    </Router>
-  </ApolloProvider>
+  <Router history={history}>
+    <div>hello, world!</div>
+  </Router>
 );
